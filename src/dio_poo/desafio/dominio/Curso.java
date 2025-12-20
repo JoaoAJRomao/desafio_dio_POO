b9456c0e8ffc66c68402f53706e118cf621a8f8e
João Arthur <tupi_1800@hotmail.com>
2025-12-20 18:48:15 -0300

    Adicionar classes dev, bootcamp e iniciar main

--- src/dio_poo/desafio/dominio/Curso.java
+++ src/dio_poo/desafio/dominio/Curso.java
@@ -1,37 +1,32 @@
 package dio_poo.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-	private String titulo;
-	private String descricao;
-	private int cargaHoraria;
-	
-	public Curso() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public int getCargaHoraria() {
-		return cargaHoraria;
-	}
-	public void setCargaHoraria(int cargaHoraria) {
-		this.cargaHoraria = cargaHoraria;
-	}
-	@Override
-	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
-	}
-	
-	
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
